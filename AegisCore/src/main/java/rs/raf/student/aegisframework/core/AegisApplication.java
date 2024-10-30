0c448373325c514fc955bcf94053b78e102d494a
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-10-31 00:22:37 +0100

    Core | implement Class Scanner

---
+++ AegisCore/src/main/java/rs/raf/student/aegisframework/core/AegisApplication.java
@@ -0,0 +1,30 @@
+package rs.raf.student.aegisframework.core;
+
+import lombok.experimental.ExtensionMethod;
+import rs.raf.student.aegisframework.core.annotation.AegisApplicationRoot;
+import rs.raf.student.aegisframework.core.scanner.ClassScanner;
+import rs.raf.student.aegisframework.utils.ansi.Attribute;
+import rs.raf.student.aegisframework.utils.ansi.Color;
+import rs.raf.student.aegisframework.utils.extension.StringANSIEscapeExtension;
+import rs.raf.student.aegisframework.utils.extension.StringBuilderExtension;
+
+@ExtensionMethod({StringBuilderExtension.class, StringANSIEscapeExtension.class})
+public class AegisApplication {
+
+    public static void run(Class<?> appClass, String[] args) {
+        AegisApplicationRoot application = appClass.getAnnotation(AegisApplicationRoot.class);
+
+        if (application == null)
+            throw new RuntimeException("Application entry point class must be annotated with @Application");
+
+        System.out.println(new StringBuilder().appendSeparatorWide()
+                                              .appendFormattedLine("     {0}",
+                                                                   application.name()
+                                                                              .applyColorAttribute(Attribute.SET_FOREGROUND, Color.NAVY)
+                                                                              .applyAttribute(Attribute.BOLD))
+                                              .appendSeparatorWide());
+
+        ClassScanner.scan(appClass.getPackageName());
+    }
+
+}
---
+++ AegisCore/src/main/java/rs/raf/student/aegisframework/core/scanner/ClassScanner.java
@@ -0,0 +1,30 @@
+package rs.raf.student.aegisframework.core.scanner;
+
+import com.google.common.collect.Sets;
+import rs.raf.student.aegisframework.utils.ClassUtils;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class ClassScanner {
+
+    private static final Set<Class<?>> loaderClasses      = ClassUtils.getAllClasses();
+    private static final Set<Class<?>> applicationClasses = Sets.newHashSet();
+
+    public static void scan(String basePackage) {
+        applicationClasses.addAll(loaderClasses.stream()
+                                               .filter(Objects::nonNull)
+                                               .filter(classInfo -> classInfo.getPackageName()
+                                                                             .startsWith(basePackage))
+                                               .toList());
+    }
+
+    public static Set<Class<?>> getClasses() {
+        return loaderClasses;
+    }
+
+    public static Set<Class<?>> getApplicationClasses() {
+        return applicationClasses;
+    }
+
+}
