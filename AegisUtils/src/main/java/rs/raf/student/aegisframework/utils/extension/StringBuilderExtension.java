588f4933883b55178d3e3707d0b24fa460f91045
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-10-30 23:39:03 +0100

    Utils | implement String Builder Line Separators

--- AegisUtils/src/main/java/rs/raf/student/aegisframework/utils/extension/StringBuilderExtension.java
+++ AegisUtils/src/main/java/rs/raf/student/aegisframework/utils/extension/StringBuilderExtension.java
@@ -4,6 +4,7 @@ import java.text.MessageFormat;
 
 public class StringBuilderExtension {
 
+
     public static StringBuilder appendLine(StringBuilder stringBuilder, String value) {
         return stringBuilder.append(value)
                             .append(System.lineSeparator());
@@ -15,6 +16,20 @@ public class StringBuilderExtension {
 
     public static StringBuilder appendFormattedLine(StringBuilder stringBuilder, String format, Object... values) {
         return appendFormatted(stringBuilder, format, values).append(System.lineSeparator());
+    }
+
+    private static final int SEPARATOR_LENGTH = 100;
+
+    public static StringBuilder appendSeparatorWide(StringBuilder stringBuilder) {
+        return appendLine(stringBuilder, "=".repeat(SEPARATOR_LENGTH));
+    }
+
+    public static StringBuilder appendSeparator(StringBuilder stringBuilder) {
+        return appendLine(stringBuilder, "-".repeat(SEPARATOR_LENGTH));
+    }
+
+    public static StringBuilder appendSeparatorThin(StringBuilder stringBuilder) {
+        return appendLine(stringBuilder, "·".repeat(SEPARATOR_LENGTH));
     }
 
 }
