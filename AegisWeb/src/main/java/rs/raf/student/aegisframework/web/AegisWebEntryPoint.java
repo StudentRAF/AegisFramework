0a766938e1cac0ab56ccb57604b2c72bf9332ef5
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-11-03 15:42:25 +0100

    Web | register library using Pipeline Manager

--- AegisWeb/src/main/java/rs/raf/student/aegisframework/web/AegisWebEntryPoint.java
+++ AegisWeb/src/main/java/rs/raf/student/aegisframework/web/AegisWebEntryPoint.java
@@ -2,12 +2,19 @@ package rs.raf.student.aegisframework.web;
 
 import rs.raf.student.aegisframework.core.annotation.AegisLibrary;
 import rs.raf.student.aegisframework.core.dependency_injection.DependencyInjectionManager;
+import rs.raf.student.aegisframework.core.pipeline.PipelineManager;
+import rs.raf.student.aegisframework.core.pipeline.StageAction;
+import rs.raf.student.aegisframework.core.pipeline.stage.CorePipelineStage;
 import rs.raf.student.aegisframework.web.dependency_injection.ControllerContainerAnnotation;
 
 @AegisLibrary
 public class AegisWebEntryPoint {
 
     static {
+        PipelineManager.register(CorePipelineStage.DEPENDENCY_INJECTION_INITIALISE_CONTAINER, StageAction.PERFORM_BEFORE, AegisWebEntryPoint::registerToDependencyInjectionManager);
+    }
+
+    private static void registerToDependencyInjectionManager() {
         DependencyInjectionManager.registerContainerAnnotation(new ControllerContainerAnnotation());
     }
 
